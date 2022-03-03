01e17aa1f7d310500c47b03c7a523b1d40ad54fb
EdsonSantana93 <edson.fs193@gmail.com>
2022-03-02 21:20:06 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/pegapelucia/peluciabrmachine/PeluciabrmachineApplication.java
@@ -0,0 +1,13 @@
+package br.com.pegapelucia.peluciabrmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluciabrmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluciabrmachineApplication.class, args);
+	}
+
+}
